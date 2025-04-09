452fe5c08293882d1230f64b59c27fbee2106192
jangtaeyeong <taeyeongj55@gmail.com>
2025-04-09 18:27:44 +0900

    Delete NSD-master directory

--- NSD-master/runme.m
+++
@@ -1,5 +0,0 @@
-
-data = load('data/dmela.mat'); A = data.A;
-data = load('data/scere.mat'); B = data.A;
-preiters = 2; iters = 10; alpha = 0.8;
-[M, G, ~, ~] = NSD_greedy(A, B, preiters, iters, alpha);
